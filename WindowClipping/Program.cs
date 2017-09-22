b7c244e80e51f42b336fadcc1c5697d8061e113e
Svend Aage Roperos Nielsen <shaddyd-sarn@live.dk>
2017-09-22 20:07:10 +0200

    Update

--- WindowClipping/Program.cs
+++ WindowClipping/Program.cs
@@ -39,6 +39,7 @@ namespace WindowClipping
         [StructLayout(LayoutKind.Sequential)]
         internal struct Win32Point
         {
+        
             public Int32 X;
             public Int32 Y;
         };
